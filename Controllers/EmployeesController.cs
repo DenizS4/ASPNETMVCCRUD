23c56b8505e514e1b36dd3e61fcd9ba147adbb50
DenizS4 <121265021+DenizS4@users.noreply.github.com>
2023-05-05 17:07:46 +0300

    Add files via upload

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,48 @@
+﻿using ASP.NETCoreEFCRUD.Data;
+using CRUDMVC.Models;
+using CRUDMVC.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDMVC.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+                Department = addEmployeeRequest.Department
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDMVC.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ASP.NETCoreEFCRUD.Data;
+
+public class MVCDemoDbContext : DbContext
+{
+    public MVCDemoDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
